317ed6961387e24bc822e508ad3b2dd21a30d88f
Satendra Baghel <143244442+satendra27@users.noreply.github.com>
2026-01-30 11:31:47 +0530

    tailwind css setup and navbar

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { FiLogOut } from "react-icons/fi";
+import { MdDashboard } from "react-icons/md";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const handleLogout = () => {
+    localStorage.clear();
+    toast.success("Logged out successfully");
+    navigate("/login");
+  };
+
+  return (
+    <nav className="bg-white shadow px-8 py-4 flex justify-between items-center">
+     <h1
+  className="text-xl font-bold text-indigo-600 flex items-center gap-2 cursor-pointer"
+  onClick={() => navigate("/dashboard")}
+>
+  <MdDashboard /> To-Do App
+</h1>
+
+      <div className="flex items-center gap-4">
+        <span className="text-gray-600">
+          👋 Hi, <strong>{user?.name}</strong>
+        </span>
+        <button
+  onClick={handleLogout}
+  className="flex items-center gap-2 bg-red-500 text-white px-4 py-1 rounded hover:bg-red-600 transition"
+>
+  <FiLogOut /> Logout
+</button>
+      </div>
+    </nav>
+  );
+}
