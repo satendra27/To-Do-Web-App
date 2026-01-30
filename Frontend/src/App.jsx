73d74bbb7b612716122b62d2cff8bdb616624fd4
Satendra Baghel <143244442+satendra27@users.noreply.github.com>
2026-01-30 11:44:08 +0530

    components called in app.jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import BoardDetails from "./pages/BoardDetails";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-const App = () => {
+function App() {
   return (
-    <div className='underline'>App</div>
-  )
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/board/:id"
+        element={
+          <ProtectedRoute>
+            <BoardDetails />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Default */}
+      <Route path="/" element={<Navigate to="/login" />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      <Toaster position="top-center" reverseOrder={false} />
+    </BrowserRouter>
+  </React.StrictMode>
+);
